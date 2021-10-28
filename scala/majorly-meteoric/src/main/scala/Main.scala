50afa701e1562db5188fdae66cbca3ceb5263a0d
harshabangi <harsha@startsmartlabs.com>
2021-10-28 17:27:12 +0530

    More changes

--- scala/majorly-meteoric/src/main/scala/Main.scala
+++ scala/majorly-meteoric/src/main/scala/Main.scala
@@ -1,17 +1,13 @@
 import ErrorHandling.ErrorRecord
 import MeteoricCfg.MeteoricRecord
-import com.amazonaws.services.s3.AmazonS3Client
 import org.apache.spark.rdd.RDD
-import org.apache.spark.{SparkConf, SparkContext}
-import org.apache.spark.sql.types.{DoubleType, IntegerType, StringType, StructField, StructType}
-import org.apache.spark.sql.{DataFrame, Row, SQLContext, SparkSession}
-import org.apache.spark.sql.functions.{col, input_file_name, lit}
+import org.apache.spark.sql.{SQLContext, SparkSession}
 
 object Main {
 
   def averageMassOfaMeteor(rdd: RDD[MeteoricCfg.MeteoricRecord]): Double = {
     val (sum, count) = rdd
-      .map(z => (z.mass.get, 1L))
+      .map(z => (z.massDouble, 1L))
       .reduce((x, y) => (x._1 + y._1, x._2 + y._2))
     sum / count
   }
@@ -33,25 +29,37 @@ object Main {
     }
   }
 
-  def doWork(sqlContext: SQLContext): Unit = {
-    val s3Bucket = "majorly-meteoric"
+  def doWork(sqlContext: SQLContext, invalidRecOutDir: String, s3Bucket: String): Unit = {
+    import sqlContext.sparkSession.implicits._
     val s3Client = new S3Client(s3Bucket)
     val rdd = sqlContext.sparkContext.parallelize(s3Client.getAllFilenames)
     val contentRdd = loadFileContents(s3Bucket, rdd).cache()
     val metRec = contentRdd.filter(x => x.isLeft).map(_.left.get)
     val invalidRec = contentRdd.filter(x => x.isRight).map(_.right.get)
+    //Save the invalid records in a file.
+    invalidRec.toDF()
+      .coalesce(1)
+      .write
+      .mode("overWrite")
+      .json(invalidRecOutDir)
     println("Average mass of a meteor: " + averageMassOfaMeteor(metRec))
-    println("Year in which highest number of meteors fall is: " + highestNumberOfMeteors(metRec))
+    println("Year in which the highest number of meteors fall is: " + highestNumberOfMeteors(metRec))
   }
 
-
+  //Entry point of the program
   def main(args: Array[String]): Unit = {
+    if (args.length < 1) {
+      System.err.println("Usage: Main <invalid-record-out-dir> <s3-bucket>(defaults to majorly-meteoric)")
+      System.exit(1)
+    }
+    val invalidRecOutDir = args(0)
+    val s3Bucket = if (args.length >= 2) args(1) else "majorly-meteoric"
     val bld = SparkSession.builder().appName("SAMPLE APP").config("spark.master", "local[*]")
     val ss = bld.getOrCreate()
     val sc = ss.sparkContext
     val sqlContext = new SQLContext(sc)
     try {
-      doWork(sqlContext)
+      doWork(sqlContext, invalidRecOutDir, s3Bucket)
     } finally {
       sc.stop()
     }
--- scala/majorly-meteoric/src/main/scala/MeteoricCfg.scala
+++ scala/majorly-meteoric/src/main/scala/MeteoricCfg.scala
@@ -3,21 +3,28 @@ import org.json4s.jackson.JsonMethods.parse
 import org.json4s.{DefaultFormats, Formats}
 
 object MeteoricCfg {
-  case class GeoLocation(latitude: Option[String], longitude: Option[String]) {
-    def isValid: Boolean = {
-      true
-    }
-  }
+  case class GeoLocation(latitude: Option[String], longitude: Option[String])
 
-  case class MeteoricRecord(name: Option[String], id: Option[String], nametype: Option[String], recclass: Option[String], mass: Option[Double], fall: Option[String],
+  case class MeteoricRecord(name: Option[String], id: Option[String], nametype: Option[String], recclass: Option[String], mass: Option[String], fall: Option[String],
                             year: Option[String], reclat: Option[String], reclong: Option[String], geolocation: GeoLocation) {
+
+    private var isValidMass = true
+    val massDouble = try {
+      mass.get.toDouble
+    } catch {
+      case _: Exception =>
+        isValidMass = false
+        0D
+    }
+
     def isValid(fileName: String): Either[MeteoricRecord, ErrorRecord] = {
       if (id.isEmpty) {
         Right(ErrorRecord("RECORD ERROR", fileName, "id column is not defined"))
       } else if (year.isEmpty) {
         Right(ErrorRecord("RECORD ERROR", fileName, "year column is not defined"))
-      } else if (mass.isEmpty) {
-        Right(ErrorRecord("RECORD ERROR", fileName, "mass column is not defined"))
+      } else if (!isValidMass) {
+        if (mass.isEmpty) Right(ErrorRecord("RECORD ERROR", fileName, "mass column is not defined"))
+        else Right(ErrorRecord("RECORD ERROR", fileName, "mass column value is invalid"))
       } else {
         Left(this)
       }
